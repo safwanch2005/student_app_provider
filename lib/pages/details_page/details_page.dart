617e526fb35097a1460dbafb9ed91736a09d983c
Safwan CH <safwan55550000@gmail.com>
2024-02-22 14:50:51 +0530

    UI almost completed

--- lib/pages/details_page/details_page.dart
+++ lib/pages/details_page/details_page.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class DetailsPage extends StatelessWidget {
   const DetailsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: Colors.deepPurple,
+      body: Container(),
     );
   }
 }
